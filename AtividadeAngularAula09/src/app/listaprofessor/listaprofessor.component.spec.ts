7791478922e94f1393651d839a8237aa18c12768
Xiaoying He <jessicahe333@hotmail.com>
2020-06-28 18:39:14 -0300

    Input & Output

---
+++ AtividadeAngularAula09/src/app/listaprofessor/listaprofessor.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaprofessorComponent } from './listaprofessor.component';
+
+describe('ListaprofessorComponent', () => {
+  let component: ListaprofessorComponent;
+  let fixture: ComponentFixture<ListaprofessorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ListaprofessorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListaprofessorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
